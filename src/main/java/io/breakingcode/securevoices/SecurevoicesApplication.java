6ceee6dba93c08569103a90abf273a61418f53a5
Scott <ScottWilliamsSolutions@gmail.com>
2024-02-28 05:50:58 -0500

    Init Project

---
+++ src/main/java/io/breakingcode/securevoices/SecurevoicesApplication.java
@@ -0,0 +1,13 @@
+package io.breakingcode.securevoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurevoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurevoicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/breakingcode/securevoices/SecurevoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.breakingcode.securevoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurevoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
